03cad8eab5de484e99efcac1fce986443e63ee42
Julien Mathis <jmathis@merethis.com>
2013-06-25 18:06:44 +0200

    first version

--- www/modules/centreon-poller-display/sql/uninstall.sql
+++ www/modules/centreon-poller-display/sql/uninstall.sql
@@ -1,6 +1,17 @@
-UPDATE topology SET topology_show = '1' WHERE topology_id = '5';
-UPDATE topology SET topology_show = '1' WHERE topology_parent = '5';
-UPDATE topology SET topology_show = '1' WHERE topology_parent => '500' AND topology_parent < '600';
-UPDATE topology SET topology_show = '1' WHERE topology_id = '6';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '6';
 UPDATE topology SET topology_show = '1' WHERE topology_parent = '6';
 UPDATE topology SET topology_show = '1' WHERE topology_parent => '600' AND topology_parent < '700';
+
+-- Administration
+UPDATE topology SET topology_show = '1' WHERE topology_page = '50101';
+UPDATE topology SET topology_show = '1' WHERE topology_parent = '50101';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '5010601';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '50104';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '50102';
+UPDATE topology SET topology_show = '1' WHERE topology_parent = '50102';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '502';
+UPDATE topology SET topology_show = '1' WHERE topology_parent = '502';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '508';
+UPDATE topology SET topology_show = '1' WHERE topology_parent = '508';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '50502';
+UPDATE topology SET topology_show = '1' WHERE topology_page = '50503';
