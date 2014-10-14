ed2f6da10e04c235bf6c5cb104009cabd4e8bd54
julienmathis <julien.mathis@gmail.com>
2014-10-14 12:17:15 +0200

    add possibility configure ACL

--- www/modules/centreon-poller-display/sql/uninstall.sql
+++ www/modules/centreon-poller-display/sql/uninstall.sql
@@ -12,8 +12,6 @@ UPDATE topology SET topology_show = '1' WHERE topology_page = '5010601';
 UPDATE topology SET topology_show = '1' WHERE topology_page = '50104';
 UPDATE topology SET topology_show = '1' WHERE topology_page = '50102';
 UPDATE topology SET topology_show = '1' WHERE topology_parent = '50102';
-UPDATE topology SET topology_show = '1' WHERE topology_page = '502';
-UPDATE topology SET topology_show = '1' WHERE topology_parent = '502';
 UPDATE topology SET topology_show = '1' WHERE topology_page = '508';
 UPDATE topology SET topology_show = '1' WHERE topology_parent = '508';
 UPDATE topology SET topology_show = '1' WHERE topology_page = '50502';
