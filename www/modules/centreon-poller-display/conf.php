4dfa3f9d1240ba40973e83c8cdd52f00e0b1549a
Julien Mathis <jmathis@merethis.com>
2016-02-15 16:45:49 +0100

    prepare 1.5.2

--- www/modules/centreon-poller-display/conf.php
+++ www/modules/centreon-poller-display/conf.php
@@ -2,11 +2,11 @@
 // Be Carefull with name, it's case sensitive (with directory module name)
 $module_conf['centreon-poller-display']["rname"] = "centreon-poller-display";
 $module_conf['centreon-poller-display']["name"] = "centreon-poller-display";
-$module_conf['centreon-poller-display']["mod_release"] = "1.5.1";
+$module_conf['centreon-poller-display']["mod_release"] = "1.5.2";
 $module_conf['centreon-poller-display']["infos"] = "centreon-poller-display";
 $module_conf['centreon-poller-display']["is_removeable"] = "1";
 $module_conf['centreon-poller-display']["author"] = "Centreon";
 $module_conf['centreon-poller-display']["lang_files"] = "0";
 $module_conf['centreon-poller-display']["sql_files"] = "1";
 $module_conf['centreon-poller-display']["php_files"] = "1";
-?>
+
