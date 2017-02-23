49a1b4b6eccf5c99082a065c254ae0b8572f6143
Kevin Duret <kduret@centreon.com>
2017-02-23 16:42:29 +0100

    fix inheritance in generate class

--- www/modules/centreon-poller-display-central/generate_files/pollerDisplay.class.php
+++ www/modules/centreon-poller-display-central/generate_files/pollerDisplay.class.php
@@ -11,16 +11,37 @@
  *
  */
 
-//require_once dirname(__FILE__) . '/subclass/export_central.class.php';
 require_once dirname(__FILE__) . '/../centreon-poller-display-central.conf.php';
+
 use \CentreonPollerDisplayCentral\ConfigGenerate\Bam\Ba;
 
 class PollerDisplay extends AbstractObject
 {
-    protected $generate_filename = 'centreon-bam-command.cfg';
+    protected $engine = false;
+    protected $broker = true;
+    protected $generate_filename = 'poller-display.sql';
+
+    public static function getInstance() {
+        static $instances = array();
+
+        $calledClass = get_called_class();
+
+        if (!isset($instances[$calledClass])) {
+            $instances[$calledClass] = new $calledClass();
+        }
+
+        return $instances[$calledClass];
+    }
+
+    public function reset() {
+
+    }
+
     public function generateFromPollerId($poller_id, $localhost) {
         $baObj = new Ba($this->backend_instance->db);
-        $baObj->generateSql();
+        $sql = $baObj->generateSql();
+
+        var_dump($this->backend_instance->getPath());
 /*
         $stmt = $this->backend_instance->db->prepare("SELECT id 
                                                     FROM mod_poller_display_server_relations 
