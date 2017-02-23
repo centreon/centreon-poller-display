72f8cff23354e0850fb65a4d3c7af325d53cd938
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-23 16:50:29 +0100

    Update centreon-poller-display-central conf.php.

--- www/modules/centreon-poller-display-central/conf.php
+++ www/modules/centreon-poller-display-central/conf.php
@@ -1,12 +1,12 @@
 <?php
 
 // Be carefull with name, it's case sensitive (with directory module name).
-$module_conf['centreon-poller-display']["rname"] = "centreon-poller-display-central";
-$module_conf['centreon-poller-display']["name"] = "centreon-poller-display-central";
-$module_conf['centreon-poller-display']["mod_release"] = "1.6.0";
-$module_conf['centreon-poller-display']["infos"] = "centreon-poller-display-central";
-$module_conf['centreon-poller-display']["is_removeable"] = "1";
-$module_conf['centreon-poller-display']["author"] = "Centreon";
-$module_conf['centreon-poller-display']["lang_files"] = "0";
-$module_conf['centreon-poller-display']["sql_files"] = "1";
-$module_conf['centreon-poller-display']["php_files"] = "0";
+$module_conf['centreon-poller-display-central']["rname"] = "centreon-poller-display-central";
+$module_conf['centreon-poller-display-central']["name"] = "centreon-poller-display-central";
+$module_conf['centreon-poller-display-central']["mod_release"] = "1.6.0";
+$module_conf['centreon-poller-display-central']["infos"] = "centreon-poller-display-central";
+$module_conf['centreon-poller-display-central']["is_removeable"] = "1";
+$module_conf['centreon-poller-display-central']["author"] = "Centreon";
+$module_conf['centreon-poller-display-central']["lang_files"] = "0";
+$module_conf['centreon-poller-display-central']["sql_files"] = "1";
+$module_conf['centreon-poller-display-central']["php_files"] = "0";
