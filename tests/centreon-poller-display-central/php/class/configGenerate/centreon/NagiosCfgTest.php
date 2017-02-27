8d9169864ed8d3067431b8b27e3f0535f5ac1a7d
Loic Laurent <loic.lau@gmail.com>
2017-02-27 16:09:31 +0100

    unit test centreon

---
+++ tests/centreon-poller-display-central/php/class/configGenerate/centreon/NagiosCfgTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright 2016 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use \Centreon\Test\Mock\CentreonDB;
+use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\NagiosCfg;
+
+/**
+ * @package centreon-poller-display-central
+ * @version 1.0.0
+ * @author Centreon
+ */
+class CentreonPollerDisplayCentral_NagiosCfg extends PHPUnit_Framework_TestCase
+{
+    protected static $db;
+    protected static $pollerDisplay;
+    protected static $nagiosCfg;
+
+    public function setUp()
+    {
+        self::$db = new CentreonDB();
+        self::$pollerDisplay = 1;
+        self::$nagiosCfg = new NagiosCfg(self::$db, self::$pollerDisplay);
+    }
+
+    public function tearDown()
+    {
+        self::$db = null;
+    }
+
+    public function testGenerateSql()
+    {
+
+        $expectedResult = 'TRUNCATE cfg_nagios;
+INSERT INTO cfg_nagios (nagios_id,nagios_name,nagios_server_id) VALUES (1,Centreon Engine,1);';
+
+        self::$db->addResultSet(
+            'SELECT * FROM cfg_nagios WHERE nagios_server_id = 1',
+            array(
+                array(
+                    'nagios_id' => '1',
+                    'nagios_name' => 'Centreon Engine',
+                    'nagios_server_id' => '1'
+                )
+            )
+        );
+
+        $sql = self::$nagiosCfg->generateSql();
+        $this->assertEquals($sql, $expectedResult);
+    }
+}
---
+++ tests/centreon-poller-display-central/php/class/configGenerate/centreon/ServiceTest.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Copyright 2016 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use \Centreon\Test\Mock\CentreonDB;
+use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\Service;
+
+
+/**
+ * @package centreon-poller-display-central
+ * @version 1.0.0
+ * @author Centreon
+ */
+class CentreonPollerDisplayCentral_Service extends PHPUnit_Framework_TestCase
+{
+    protected static $db;
+    protected static $pollerDisplay;
+    protected static $service;
+
+    public function setUp()
+    {
+        self::$db = new CentreonDB();
+        self::$pollerDisplay = 1;
+        self::$service = new Service(self::$db, self::$pollerDisplay);
+    }
+
+    public function tearDown()
+    {
+        self::$db = null;
+    }
+
+    public function testGenerateSql()
+    {
+
+        $expectedResult = 'TRUNCATE service;
+INSERT INTO service (service_id,service_activate) VALUES (1,1);';
+
+        self::$db->addResultSet(
+            'SELECT * FROM ns_host_relation WHERE nagios_server_id = 1',
+            array(
+                array(
+                    'nagios_server_id' => '1',
+                    'host_host_id' => '1'
+                ),
+                array(
+                    'nagios_server_id' => '1',
+                    'host_host_id' => '2'
+                )
+            )
+        );
+
+        self::$db->addResultSet(
+            'SELECT * FROM hostgroup_relation WHERE host_host_id IN (1,2)',
+            array(
+                array(
+                    'hgr_id' => '1',
+                    'hostgroup_hg_id' => '1',
+                    'host_host_id' => '1'
+                )
+            )
+        );
+
+        self::$db->addResultSet(
+            'SELECT * FROM host_service_relation WHERE (host_host_id IN (1,2)) OR (hostgroup_hg_id IN (1))',
+            array(
+                array(
+                    'hsr_id' => '1',
+                    'hostgroup_hg_id' => null,
+                    'host_host_id' => '1',
+                    'servicegroup_sg_id' => null,
+                    'service_service_id' => '1'
+                )
+            )
+        );
+
+        self::$db->addResultSet(
+            'SELECT * FROM service WHERE service_id IN (1)',
+            array(
+                array(
+                    'service_id' => '1',
+                    'service_activate' => '1'
+                )
+            )
+        );
+
+
+        $sql = self::$service->generateSql();
+        $this->assertEquals($sql, $expectedResult);
+    }
+}
