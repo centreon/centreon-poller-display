2b2209e0e6788d45782dede84e11d42477ebd7df
Loic Laurent <loic.lau@gmail.com>
2017-03-06 18:04:37 +0100

    add check if file exist and count array

--- cron/centreon-poller-display-sync.sh
+++ cron/centreon-poller-display-sync.sh
@@ -6,29 +6,26 @@ DB_CENTREON=$(grep '\'db\' /etc/centreon/centreon.conf.php | awk -F= '{ print $2
 
 FILE_PATH="/etc/centreon-broker/"
 HASH_PATH="/etc/centreon/"
-configNameFiles[0]='centreon-poller-display'
-configNameFiles[1]='bam-poller-display'
+config_name_files=( "centreon-poller-display" "bam-poller-display" )
 extend_save='-save.txt'
 extend_sql='.sql'
 
-for ((i=0; i < ${#configNameFiles}; i++))
+for ((i=0; i < ${#config_name_files[@]}; i++))
 do
-
-SQL_FILE=$FILE_PATH${configNameFiles[$i]}$extend_sql
-HASH_FILE=$HASH_PATH${configNameFiles[$i]}$extend_save
-
-    if [ -f $HASH_FILE ] ; then
-        hash_content=`cat $HASH_FILE`
-    else
-        hash_content=''
-    fi
-
-filecontent=`cat $SQL_FILE`
-hashFile=`md5sum < $HASH_FILE`
-
-    if [ $hash_content != $hashFile ] ; then
-        mysql --user="$USER" --password="$PASSWORD" --database="$DB_CENTREON" --execute="$filecontent"
-        echo $hashFile > $HASH_FILE
-        service cbd restart
-    fi
+SQL_FILE=${FILE_PATH}${config_name_files[$i]}${extend_sql}
+    if [ -f ${SQL_FILE} ] ; then
+    HASH_FILE=${HASH_PATH}${config_name_files[$i]}${extend_save}
+        if [ -f ${HASH_FILE} ] ; then
+            hash_content=`cat ${HASH_FILE}`
+        else
+            hash_content=''
+        fi
+    file_content=`cat ${SQL_FILE}`
+    hashFile=`md5sum < ${HASH_FILE}`
+        if [ "${hash_content}" != "${hashFile}" ] ; then
+            mysql --user="$USER" --password="$PASSWORD" --database="$DB_CENTREON" --execute="$file_content"
+            echo ${hashFile} > ${HASH_FILE}
+            service cbd restart
+        fi
+   fi
 done
