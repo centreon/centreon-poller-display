7f9a43a7138efc13ac637775739204b15b7b7b2b
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-20 12:19:13 +0100

    Run centreon-poller-display-sync as root.

--- config/centreon-poller-display.cron.d
+++ config/centreon-poller-display.cron.d
@@ -1,2 +1,2 @@
 # Cron Config File for centreon-poller-display
-* * * * * centreon /usr/share/centreon/cron/centreon-poller-display-sync.sh >> /var/log/centreon/centreon-poller-display.log 2>&1
+* * * * * root /usr/share/centreon/cron/centreon-poller-display-sync.sh >> /var/log/centreon/centreon-poller-display.log 2>&1
