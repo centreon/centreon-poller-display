3b4bbc04f932ca91de72bcf1d1f277d89de09519
loiclau <loic.lau@gmail.com>
2017-07-20 11:15:57 +0200

    * export topology

--- www/modules/centreon-poller-display-central/core/class/configGenerate/Centreon.php
+++ www/modules/centreon-poller-display-central/core/class/configGenerate/Centreon.php
@@ -41,6 +41,9 @@ use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclGroupActionsRelatio
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclGroupContactgroupsRelation;
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclGroupContactsRelation;
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclGroupTopology;
+use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclTopology;
+use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclTopologyRelation;
+use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\Topology;
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclGroups;
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclResources;
 use \CentreonPollerDisplayCentral\ConfigGenerate\Centreon\AclResourcesGroupRelation;
@@ -117,6 +120,8 @@ class Centreon extends \AbstractObject
         $oAclGroupContactgroupsRelation = new AclGroupContactgroupsRelation($db, $poller_id);
         $oAclGroupContactsRelation = new AclGroupContactsRelation($db, $poller_id);
         $oAclGroupTopology = new AclGroupTopology($db, $poller_id);
+        $oAclTopology = new AclTopology($db, $poller_id);
+        $oAclTopologyRelation = new AclTopologyRelation($db, $poller_id);
         $oAclGroups = new AclGroups($db, $poller_id);
         $oAclResources = new AclResources($db, $poller_id);
         $oAclResourcesGroupRelation = new AclResourcesGroupRelation($db, $poller_id);
@@ -158,9 +163,11 @@ class Centreon extends \AbstractObject
         $oServiceInformation = new ServiceInformation($db, $poller_id);
         $oServicegroup = new Servicegroup($db, $poller_id);
         $oServicegroupRelation = new ServicegroupRelation($db, $poller_id);
+        $oTopology = new Topology($db, $poller_id);
+
 
         $sql = '';
-        $sql .= $this->setForeignKey(0). "\n\n";
+        $sql .= $this->setForeignKey(0) . "\n\n";
 
         $nagiosServerList = $oNagiosServer->getList();
         $sql .= $oNagiosServer->generateSql($nagiosServerList) . "\n\n";
@@ -287,6 +294,15 @@ class Centreon extends \AbstractObject
 
         $aclGroupTopologyList = $oAclGroupTopology->getList($aclGroupsList);
         $sql .= $oAclGroupTopology->generateSql($aclGroupTopologyList) . "\n\n";
+
+        $aclTopologyList = $oAclTopology->getList($aclGroupTopologyList);
+        $sql .= $oAclTopology->generateSql($aclTopologyList) . "\n\n";
+
+        $aclTopologyRelationList = $oAclTopologyRelation->getList($aclTopologyList);
+        $sql .= $oAclGroupTopology->generateSql($aclTopologyRelationList) . "\n\n";
+
+        $topologyList = $oTopology->getList($aclTopologyRelationList);
+        $sql .= $oTopology->generateSql($topologyList) . "\n\n";
 
         $aclGroupActionsRelationList = $oAclGroupActionsRelation->getList($aclGroupsList);
         $sql .= $oAclGroupActionsRelation->generateSql($aclGroupActionsRelationList) . "\n\n";
